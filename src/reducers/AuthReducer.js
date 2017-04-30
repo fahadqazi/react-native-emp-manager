911852ed41b0a7da4fda37b2fce2f8c236a9b456
Fahad Qazi <fahadqazi@gmail.com>
2017-04-30 23:11:38 +0100

    setting up reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -4,8 +4,8 @@ const INITIAL_STATE = { email: '' };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'email_changed':
-
+        case EMAIL_CHANGED:
+            return { ...state, email: action.payload };
 
         default:
             return state;  
