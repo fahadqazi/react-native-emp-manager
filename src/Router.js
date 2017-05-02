5ac470b64e1d7159b9a0890257563f007fb2b87b
Fahad Qazi <fahadqazi@gmail.com>
2017-05-02 22:20:27 +0100

    navigation added

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
-const RouterComponenet = () => {
+const RouterComponent = () => {
     return (
-        <Router>
-            <Scene key="login" component={LoginForm} title="Please Login" />
+        <Router sceneStyle={{ paddingTop: 65 }}>
+
+            <Scene key="auth">
+                <Scene key="login" component={LoginForm} title="Please login" />
+            </Scene>
+
+            <Scene key="main">
+                <Scene 
+                onRight={() => Actions.employeeCreate()}
+                rightTitle="Add"
+                key="employeeList" 
+                component={EmployeeList} 
+                title="Employee List" 
+                initial
+                />
+                <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+            </Scene>
         </Router>
     );
 };
 
-export default RouterComponenet;
+export default RouterComponent;
