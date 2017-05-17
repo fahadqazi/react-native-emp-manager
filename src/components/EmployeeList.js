ae2c9eef6bcbc9835fbed368ca7afd4169c28493
Fahad Qazi <fahadqazi@gmail.com>
2017-05-17 23:44:58 +0100

    setting up component to render list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
-import { Text, View } from 'react-native';
+import { ListView, Text, View } from 'react-native';
 
 class EmployeeList extends Component {
     componentWillMount(){
         this.props.employeesFetch();
+
+        createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps){
+        createDataSource(nextProps);
+    }
+
+    createDataSource = ({ employees }) => {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
