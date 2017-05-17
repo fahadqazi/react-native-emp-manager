ccd26fc2c1005bacbead668e1c4c08b796c08a78
Fahad Qazi <fahadqazi@gmail.com>
2017-05-17 22:44:38 +0100

    adding action creator to fetch data employee list data

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -27,3 +27,13 @@ export const employeeCreate = ({ name, phone, shift }) => {
     };
 };
 
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+            });
+    };
+};
