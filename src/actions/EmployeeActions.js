f7801b34eb95d97b92a9f39d5d74c3177f2218c7
Fahad Qazi <fahadqazi@gmail.com>
2017-05-17 23:06:15 +0100

    setting up reducer of fetching employees

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -27,13 +28,13 @@ export const employeeCreate = ({ name, phone, shift }) => {
     };
 };
 
-export const employeeFetch = () => {
+export const employeesFetch = () => {
     const { currentUser } = firebase.auth();
 
     return (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
             .on('value', snapshot => {
-                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             });
     };
 };
